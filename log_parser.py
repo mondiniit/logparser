c5a07e023ae292e5e0d873a2bb18e382c0c849a7
dmondini <dg.mondini@gmail.com>
2020-06-01 15:50:29 -0400

    [DM]feat: last.

--- log_parser.py
+++ log_parser.py
@@ -1,28 +1,32 @@
 import re
-import time
-from time import strftime
 from datetime import datetime
 
- 
+
+def get_iso_datetime() :
+    return datetime.now().isoformat()
+
+
+def replace_file_times(log_file_path):
+    pattern = re.compile(r'(UTC\s[A-Z][a-z]*\s[0-9]*\s[0-9]*:[0-9]*:[0-9]*)')
+    current_time = get_iso_datetime()
+    with open(log_file_path, 'r') as f:
+        file_lines = f.readlines()
+        f.seek(0)
+        out = f.read()
+        # print(tempFile.read())
+        for line in file_lines:
+            matches = re.findall(pattern, line)
+            for match in matches:
+                out = out.replace(match, current_time)
+    with open(log_file_path, 'w') as f:
+        f.write(out)
+    return out
+
+
 def main():
-    log_file_path = r"/Users/mondini/Documents/Falabella/log_parser/out.txt"
-    export_file_path = r"/Users/mondini/Documents/Falabella/log_parser/out.log"
- 
-    time_now = datetime.now()
-    export_file = open(export_file_path, "wt")
- 
-    regex = '(UTC\s[A-Z][a-z]*\s[0-9]*\s[0-9]*:[0-9]*:[0-9]*)'
-    timeIso = time_now.isoformat()
-    parseData(log_file_path, export_file, regex, timeIso)
- 
- 
- 
-def parseData(log_file_path, export_file, regex, timeIso):
-    file = open(log_file_path,("rw"))
-    for i, line in enumerate(open(file)):
-        data = file.read()
-        data.replace(timeIso, regex)
-        export_file.write(data)
-            
+    log_file_path = "out.txt"
+    print(replace_file_times(log_file_path))
+
+
 if __name__ == '__main__':
     main()
